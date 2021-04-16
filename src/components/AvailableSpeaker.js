fd56892601b6ed60bbd87b8d8117b46ccebabd95
CasperK97 <casper.kroes@hetnet.nl>
2021-04-16 11:40:52 +0200

    add dropdown

---
+++ src/components/AvailableSpeaker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AvailableSpeaker = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AvailableSpeaker
--- src/components/BodyColumns.js
+++ src/components/BodyColumns.js
@@ -4,13 +4,15 @@ import SpeakerTimer from './SpeakerTimer.js'
 import TotTimer from './TotTimer.js'
 import ButtonList from './ButtonList.js'
 import InputFields from './InputFields.js'
+import SpeakerSelector from './SpeakerSelector.js'
 
 const BodyColumns = ({speakers, onDelete}) => {
     return (
         <div className="hero" id="BodyColumns">
             <div className="columns">
-                <div className="column"> 
+                <div className="column is-one-fifth"> 
                     <p>Speakers</p>
+                    <SpeakerSelector />
                     {speakers.length > 0 ? <SpeakerList speakers={speakers} onDelete={onDelete}/> : <h3>"No speakers left"</h3>}
                 </div>
                 <div className="column is-one-third">
