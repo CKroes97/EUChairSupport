67a17a8af8d6ca8fb0b92e4c9caf565d64400a9d
CasperK97 <casper.kroes@hetnet.nl>
2021-04-22 20:59:30 +0200

    add documentation

--- src/components/BodyColumns/Speaker.js
+++ src/components/BodyColumns/Speaker.js
@@ -4,6 +4,7 @@ import './Speaker.css'
 const speaker = ({speaker, fastForward, setIsRunning, setActiveSpeaker, resetSpeaker}) => {
     return (
         <div className='speaker' id='speaker'>
+            {/* fugly way of checking whether it is in FullList or SpeakerList, organise later */}
             <h3 onDoubleClick={fastForward ? () => {fastForward(speaker.id);setIsRunning(true)} : () => {setActiveSpeaker(speaker.country);setIsRunning(true);resetSpeaker()} }>{speaker.country}</h3>
         </div>
     )
